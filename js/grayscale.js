f9ab028ab8a3252319f719aca6635bbab37cfe87
VirusKA <devaprise@outlook.com>
2014-10-10 17:47:44 -0400

    Update

--- js/grayscale.js
+++ js/grayscale.js
@@ -4,27 +4,16 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-// jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
+  50 < $(".navbar").offset().top ? $(".navbar-fixed-top").addClass("top-nav-collapse") : $(".navbar-fixed-top").removeClass("top-nav-collapse");
 });
-
-// jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+  $("a.page-scroll").bind("click", function(a) {
+    var b = $(this);
+    $("html, body").stop().animate({scrollTop:$(b.attr("href")).offset().top}, 1500, "easeInOutExpo");
+    a.preventDefault();
+  });
 });
-
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+$(".navbar-collapse ul li a").click(function() {
+  $(".navbar-toggle:visible").click();
 });
